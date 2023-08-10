4e85ed319b5ce8a1c27bc2ab322e2c1a8b1ee8a8
geekhub330 <geek330@xauat.edu.cn>
2023-08-10 18:06:51 +0800

    Add judgment, whether it is a message from cmd_vel or not

--- diablo_interaction/diablo_convert/src/msg_convert.cpp
+++ diablo_interaction/diablo_convert/src/msg_convert.cpp
@@ -9,101 +9,74 @@ public:
   MsgConvert() : Node("msg_convert")
   {
     // 创建一个发布器来发布自定义消息
-    motion_cmd_pub_ = this->create_publisher<motion_msgs::msg::MotionCtrl>("diablo/MotionCmd", 10);
+    motion_cmd_pub = this->create_publisher<motion_msgs::msg::MotionCtrl>("diablo/MotionCmd", 10);
     // 创建一个订阅器来订阅cmd_vel消息
-    cmd_vel_sub_ = 
+    cmd_vel_sub = 
       this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&MsgConvert::msgconvert_callback, this, std::placeholders::_1));
+    //创建一个节点接收键盘控制的消息
     teleop_cmd_sub = 
-      this->create_subscription<motion_msgs::msg::MotionCtrl>("teleop_motion_cmd", 10, std::bind(&MsgConvert::teleop_motion_cmd_callback, this, std::placeholders::_1));
-    //create a timer to publish the custom message every 30ms
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(30), std::bind(&MsgConvert::timer_callback, this));
+      this->create_subscription<motion_msgs::msg::MotionCtrl>("key_control", 10, std::bind(&MsgConvert::key_control_callback, this, std::placeholders::_1));
+    //创建一个定时器每隔50ms(20hz)发布一次速度信息
+    timer = this->create_wall_timer(std::chrono::milliseconds(50), std::bind(&MsgConvert::timer_callback, this));
     
-    custom_msg.mode_mark = false;
-
-    custom_msg.value.forward = 0.0;
-    custom_msg.value.left = 0.0;
-    custom_msg.value.up = 0.0;
-    custom_msg.value.roll = 0.0;
-    custom_msg.value.pitch = 0.0;
-    custom_msg.value.leg_split = 0.0;
-
-    custom_msg.mode.pitch_ctrl_mode = false;
-    custom_msg.mode.roll_ctrl_mode = false;
-    custom_msg.mode.height_ctrl_mode = false;
-    custom_msg.mode.stand_mode = false;
-    custom_msg.mode.jump_mode = false;
-    custom_msg.mode.split_mode = false;
-
-    last_custom_msg = custom_msg;
-    i = 0;
+  
   }
 
 private:
-  motion_msgs::msg::MotionCtrl custom_msg;
-  motion_msgs::msg::MotionCtrl last_custom_msg;
-  int i;
+  motion_msgs::msg::MotionCtrl vel_msg;
+  motion_msgs::msg::MotionCtrl key_msg;
 
   void msgconvert_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
   {
-    // assign the value of the incoming msg to custom_msg
-    custom_msg.value.forward = msg->linear.x;
-    custom_msg.value.left = msg->angular.z;
+    // 将传进来的cmd_vel速度信息赋值给自定义消息
+    vel_msg.value.forward = msg->linear.x;
+    vel_msg.value.left = msg->angular.z;
+    RCLCPP_INFO(this->get_logger(), "start cmd_vel -> motion_msgs");
   }
 
-  void teleop_motion_cmd_callback(const motion_msgs::msg::MotionCtrl::SharedPtr msg)
+  //接收键盘的回调消息
+  void key_control_callback(const motion_msgs::msg::MotionCtrl::SharedPtr msg)
   {
-    last_custom_msg.mode_mark = custom_msg.mode_mark;
-    custom_msg.mode_mark = msg->mode_mark;
-    // if the custom_msg.mode_mark change from false to true, we want to keep the custom_msg.mode_mark true for 3 times
-    if (custom_msg.mode_mark != last_custom_msg.mode_mark)
-    {
-      custom_msg.mode_mark = true;
-      i++;
-      if (i > 10)
-      {
-        custom_msg.mode_mark = false;
-        i = 0;
-      }
-    }
-
-
-    // // if the value of the custom_msg.mode is not equal to the last_custom_msg.mode
-    // // we should assign the custom_msg.mode_mark to true and last for 5 times
-    // if (custom_msg.mode.pitch_ctrl_mode != last_custom_msg.mode.pitch_ctrl_mode ||
-    //     custom_msg.mode.roll_ctrl_mode != last_custom_msg.mode.roll_ctrl_mode ||
-    //     custom_msg.mode.height_ctrl_mode != last_custom_msg.mode.height_ctrl_mode ||
-    //     custom_msg.mode.stand_mode != last_custom_msg.mode.stand_mode ||
-    //     custom_msg.mode.jump_mode != last_custom_msg.mode.jump_mode ||
-    //     custom_msg.mode.split_mode != last_custom_msg.mode.split_mode)
-    // {
-    //   custom_msg.mode_mark = true;
-    //   i = 0;
-    // }
-    // else
-    // {
-    //   i++;
-    //   if (i > 5)
-    //   {
-    //     custom_msg.mode_mark = false;
-    //   }
-    // }
+    key_msg.value = msg->value;
+    key_msg.mode = msg->mode;
+    key_msg.mode_mark = msg->mode_mark;
+    key_msg.emergency_mode = msg->emergency_mode;
     
-    // assign the incoming msg.mode to custom_msg.mode
-    custom_msg.mode = msg->mode;
-    custom_msg.value.up = msg->value.up;
-    custom_msg.value.pitch = msg->value.pitch;
   }
 
   void timer_callback()
   {
-    // publish the custom_msg
-    motion_cmd_pub_->publish(custom_msg);
+    
+    if (!key_msg.emergency_mode)//判断当前是否为紧急模式
+    {
+      if(vel_msg.value.forward != 0.0 || vel_msg.value.left != 0.0)//导航模式下只发布cmd_vel的速度消息，其它模式与键盘设置后的机器人姿态保持一致
+      {
+        vel_msg.value.up = key_msg.value.up;
+        vel_msg.value.pitch = key_msg.value.pitch;
+        vel_msg.mode_mark = key_msg.mode_mark;
+        vel_msg.mode = key_msg.mode;
+        vel_msg.emergency_mode = key_msg.emergency_mode;
+        motion_cmd_pub->publish(vel_msg);
+      }
+      else
+      {
+        motion_cmd_pub->publish(key_msg);
+      }
+        
+      
+    }
+    else//紧急模式下只可用键盘控制发布消息
+    {
+      motion_cmd_pub->publish(key_msg);
+      
+    }
+    
     
   }
-  rclcpp::Publisher<motion_msgs::msg::MotionCtrl>::SharedPtr motion_cmd_pub_;
+  rclcpp::Publisher<motion_msgs::msg::MotionCtrl>::SharedPtr motion_cmd_pub;
   rclcpp::Subscription<motion_msgs::msg::MotionCtrl>::SharedPtr teleop_cmd_sub;
-  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
-  rclcpp::TimerBase::SharedPtr timer_; 
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
+  rclcpp::TimerBase::SharedPtr timer; 
 } ;
 
 int main(int argc, char** argv)
