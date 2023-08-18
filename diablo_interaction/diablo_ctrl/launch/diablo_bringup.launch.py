45bda18b70a259f61d9d2451862fff67abdc748d
geekhub330 <geek330@xauat.edu.cn>
2023-08-18 11:37:13 +0800

    restore launch file

--- diablo_interaction/diablo_ctrl/launch/diablo_bringup.launch.py
+++ diablo_interaction/diablo_ctrl/launch/diablo_bringup.launch.py
@@ -7,12 +7,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    point_to_laser_dir = os.path.join(get_package_share_directory('pointcloud_to_laserscan'), 'launch',)
-    livox_dir = os.path.join(get_package_share_directory('livox_ros_driver2'), 'launch_ROS2',)
-    IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([livox_dir, '/msg_MID360_launch.py']),
-        PythonLaunchDescriptionSource([point_to_laser_dir, '/sample_pointcloud_to_laserscan_launch.py']),  
-    )
     return LaunchDescription(
         [
             
