87e2b0c81f955724693bedac7f081bcaa0522d09
geekhub330 <geek330@xauat.edu.cn>
2023-08-18 16:16:01 +0800

    add lidar launch description

--- diablo_interaction/diablo_ctrl/launch/diablo_bringup_launch.py
+++ diablo_interaction/diablo_ctrl/launch/diablo_bringup_launch.py
@@ -7,9 +7,39 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    livox_dir = os.path.join(get_package_share_directory('livox_ros_driver2'),'launch_ROS2')
+    
     return LaunchDescription(
         [
-            
+        IncludeLaunchDescription(
+        	PythonLaunchDescriptionSource([livox_dir,'/msg_MID360_launch.py'])
+        	),
+            Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher',
+            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'livox_frame']
+        ),
+            Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/livox/lidar'),
+                        ],
+            parameters=[{
+                'target_frame': 'livox_frame',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 0.1,
+                'angle_min': -3.1415,  # -M_PI/2
+                'angle_max': 3.1415,  # M_PI/2
+                'angle_increment': 0.005,  # M_PI/360.0
+                'scan_time': 0.1,
+                'range_min': 0.1,
+                'range_max': 30.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        ),
             Node(
                 package = 'diablo_convert',
                 executable = 'msg_convert_node',
