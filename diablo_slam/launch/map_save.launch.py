19f8924c46c79baaee2d8c9154f1827a7bb6ae1b
sujit-168 <su2054552689@gmail.com>
2024-08-08 15:07:32 +0800

    feat: add diablo_slam

--- diablo_slam/launch/map_save.launch.py
+++ diablo_slam/launch/map_save.launch.py
@@ -3,16 +3,9 @@ from pathlib import Path
 import time
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import TextSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
-    # get map name from args
-    # map_filename_launch_arg = DeclareLaunchArgument(
-    #     "map_filename", default_value=TextSubstitution(text="")
-    # )
 
     # map named is map+current time
     current_time = time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
@@ -32,17 +25,14 @@ def generate_launch_description():
     map_save_config = os.path.join(map_directory, map_filename)
 
     return LaunchDescription(
-        [   
-            # map_filename_launch_arg,
-            # DeclareLaunchArgument("map_saver_params_file", default_value=map_save_config, description="Map Saver Configuration File"),
-
+        [
             Node(
                 package='nav2_map_server',
                 executable='map_saver_cli',
                 name='map_saver_cli',
-                # namespace='your_namespace',
-                # arguments=['-r', '__ns:=/your_namespace', '-f', map_filename],
-                arguments=['-t', 'map', '-f', map_save_config],
+                arguments=['-t', 'map',
+                           '-f', map_save_config
+                        ],
                 output='screen'
             )
         ]
